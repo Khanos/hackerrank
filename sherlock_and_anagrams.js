1cc0dafd52bde3ff83253c0cccca43e169ed8bfe
khanos <epilef.rodriguez@gmail.com>
2019-06-15 20:30:01 +0200

    this is drivin me crazy

--- sherlock_and_anagrams.js
+++ sherlock_and_anagrams.js
@@ -17,26 +17,62 @@
 // let s = randomStr({size: 12, type: ['letters']});
 // console.log('result', sherlockAndAnagrams('abcd'));
 
-let randomStr = require('./utils/random-str');
-
-function sherlockAndAnagrams(s) {
-    let word = '';
-    let rest = '';
-    let count = 0;
-    let anagramsArray = []
-    for (let i = 0; i<s.lenght ; i++){
-        console.log(s[i],'==')
-        for(let j = i+1; j<s.lenght; j++){
-            console.log(s[i],'==', s[j])
-            if (s[i] == s[j]){
-                anagramsArray.push([i,j]);
-                break;
-            }
-        }
+// let randomStr = require('./utils/random-str');
+function getIndexArray(start, length){
+    let arr = [];
+    for(let i=0;i<length;i++){
+        arr.push(i+start);
     }
-    return anagramsArray;
+    return arr;
 }
 
+function findAnagram(str, s, rootIndex){
+    let response = [];
+    let findIt;
+    for (let index = 0; index + str.length <= s.length; index++) {
+        let newStr = s.slice(index, rootIndex+index+str.length);
+        findIt = true;
+        //console.log("Esta ", str, " dentro de: ", newStr)
+        for (let i = 0; i < str.length; i++) {
+            if(newStr.indexOf(str[i]) == -1){
+                findIt = false;
+                break;
+            }
+            newStr = newStr.slice(0, newStr.indexOf(str[i])) + newStr.slice(newStr.indexOf(str[i]) + 1, newStr.length);
+        }
+        if (findIt){
+            response.push(getIndexArray(rootIndex, str.length), getIndexArray(rootIndex+index+str.length, str.length))
+        }
+    }
+    return response;
+}
+// console.log("for ba");
+// console.log(findAnagram("b", "a", 0))
 
-//let s = randomStr({size: 100, type: ['letters']});
-console.log('result', sherlockAndAnagrams('abba'));
+function sherlockAndAnagrams(s) {
+    let finish = false;
+    let tempArr = false;
+    let anagramsArray = [];
+    let n = 1;
+    while (!finish){
+        for (let i = 0; i + n < s.length; i++){
+            anagram = s.slice(i,i+n);
+            stringToSearch = s.slice(i+1,s.length);
+            tempArr = findAnagram(anagram, stringToSearch, i)
+            console.log("Esta ", anagram, " dentro de: ", stringToSearch, " ? ", tempArr)
+            if (tempArr.length > 0){
+                anagramsArray.push(tempArr);
+            }
+        }
+        console.log("---------------------------")
+        n++;
+        if(n>=s.length){
+            finish = true;
+        }
+    }
+    //console.log("----------------------------")
+    console.log(anagramsArray)
+    return anagramsArray.length;
+}
+// sherlockAndAnagrams("abba");
+console.log(sherlockAndAnagrams("kkkk"));
