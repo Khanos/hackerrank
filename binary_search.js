cda080f69539011188bf21741c7a45328c90940e
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2019-08-28 13:43:46 +0200

    optimizing the search, by filtering both ends

--- binary_search.js
+++ binary_search.js
@@ -1,5 +1,3 @@
-let mylist = [0, 1, 2, 5, 7, 3, 8, 10, 14, 11, 19, 21, 15, 16, 12, 27, 13, 17];
-let value = 19
 // My first attempt, no cheating
 function binarysearch(list, value, isOrdered){
     if (!isOrdered){
@@ -15,9 +13,19 @@ function binarysearch(list, value, isOrdered){
     let left = list.slice(0, Math.round(list.length/2));
     let right = list.slice(Math.round(list.length/2), list.length);
     if(value <= left[left.length-1]){
-        return binarysearch(left, value, true);
+        if(value == left[0] || value == left[left.length-1]){
+            return value+" was found!"
+        } else {
+            return binarysearch(left, value, true);
+        }
     } else {
-        return binarysearch(right, value, true);
+        if(value == right[0] || value == right[right.length-1]){
+            return value+" was found!"
+        } else {
+            return binarysearch(right, value, true);
+        }
     }
 }
- console.log(binarysearch(mylist, 17, false));
+let mylist = [0, 1, 2, 5, 7, 3, 8, 10, 14, 11, 19, 21, 15, 16, 12, 27, 13, 17];
+let value = 0
+console.log(binarysearch(mylist, value, false));
