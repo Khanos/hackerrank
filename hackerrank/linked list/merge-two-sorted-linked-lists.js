462285b4be57d81275f45bd6827d17d2bdc62587
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-07-18 11:37:44 -0400

    Done: https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem

--- hackerrank/linked list/merge-two-sorted-linked-lists.js
+++ hackerrank/linked list/merge-two-sorted-linked-lists.js
@@ -1,24 +1,24 @@
 class SinglyLinkedListNode {
     constructor(data){
-       this.data = data;
-       this.next = null;
-    }
-    addNode(node, data){
-        if(node.next == null){
-            node.next = new SinglyLinkedListNode(data)
-        } else {
-            this.addNode(node.next, data);
-        }
-        return node;
-    }
+        this.data = data;
+        this.next = null;
+     }
+     insertNode(node, data){
+         if(node.next == null){
+             node.next = new SinglyLinkedListNode(data)
+         } else {
+             this.insertNode(node.next, data);
+         }
+         return node;
+     }
 }
 
 let llist1 = new SinglyLinkedListNode(1);
-llist1.addNode(llist1, 2);
-llist1.addNode(llist1, 3);
+llist1.insertNode(llist1, 2);
+llist1.insertNode(llist1, 3);
 
 let llist2 = new SinglyLinkedListNode(3);
-llist2.addNode(llist2, 4);
+llist2.insertNode(llist2, 4);
 
 console.log('---------------------------------------------------------------');
 // Complete the mergeLists function below.
@@ -33,14 +33,6 @@ console.log('---------------------------------------------------------------');
  *
  */
 function mergeLists(head1, head2) {
-    function addNode(node, data){
-        if(node.next == null){
-            node.next = new SinglyLinkedListNode(data)
-        } else {
-            addNode(node.next, data);
-        }
-        return node;
-    }
     let mergedList;
     let keppGoing = true;
     if(head1.data > head2.data){
@@ -52,28 +44,22 @@ function mergeLists(head1, head2) {
     }
     while(keppGoing){
         if(head1 !== null && head2 !== null){
-            if(head1.data === head2.data){
-                mergedList = addNode(mergedList, head1.data);
-                head1 = head1.next;
+            if(head1.data > head2.data){
+                mergedList.insertNode(mergedList, head2.data);
                 head2 = head2.next;
             } else {
-                if(head1.data > head2.data){
-                    mergedList = addNode(mergedList, head2.data);
-                    head2 = head2.next;
-                } else {
-                    mergedList = addNode(mergedList, head1.data);
-                    head1 = head1.next;
-                }
+                mergedList.insertNode(mergedList, head1.data);
+                head1 = head1.next;
             }
         } else {
             if(head1 === null && head2 === null){
                 keppGoing = false;
             } else {
                 if(head1 !== null){
-                    mergedList = addNode(mergedList, head1.data);
+                    mergedList.insertNode(mergedList, head1.data);
                     head1 = head1.next;
                 } else {
-                    mergedList = addNode(mergedList, head2.data);
+                    mergedList.insertNode(mergedList, head2.data);
                     head2 = head2.next;
                 }
             }
