6038a1a3103a5ad35ca5813095c99a8bc0c2c944
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-07-19 06:35:20 -0400

    Done: https://www.hackerrank.com/challenges/reverse-a-linked-list/problem

---
+++ hackerrank/linked list/reverse-a-linked-list.js
@@ -0,0 +1,49 @@
+class SinglyLinkedListNode {
+    constructor(data){
+       this.data = data;
+       this.next = null;
+    }
+    addNode(node, data){
+        if(node.next == null){
+            node.next = new SinglyLinkedListNode(data)
+        } else {
+            this.addNode(node.next, data);
+        }
+        return node;
+    }
+}
+
+let llist = new SinglyLinkedListNode(1);
+llist.addNode(llist, 2);
+llist.addNode(llist, 3);
+llist.addNode(llist, 4);
+llist.addNode(llist, 5);
+
+console.log('---------------------------------------------------------------');
+// Complete the reverse function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function reverse(head) {
+    console.log(JSON.stringify(head));
+    let prev = null;
+    let curr = head;
+    let next = null;
+    while(curr){
+        next = curr.next;
+        curr.next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+console.log('---------------------------------------------------------------');
+let result = reverse(llist);
+console.log(JSON.stringify(result));
