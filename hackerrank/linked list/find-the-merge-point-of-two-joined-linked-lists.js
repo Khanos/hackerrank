10296c8494b01b4b068d0eb41172e35e8bcca842
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-07-23 06:32:47 -0400

    Not even with promises the hackerrank accepts it

--- hackerrank/linked list/find-the-merge-point-of-two-joined-linked-lists.js
+++ hackerrank/linked list/find-the-merge-point-of-two-joined-linked-lists.js
@@ -8,15 +8,15 @@ llist.insertNode(3);
 
 let llist2 = new SinglyLinkedList();
 llist2.insertNode(1);
-llist2.insertNode(2);
 llist2.insertNode(3);
 
 let llist3 = new SinglyLinkedList();
+llist3.insertNode(3);
 llist3.insertNode(4);
 
 let llist4 = new SinglyLinkedList();
 llist4.insertNode(1);
-
+llist4.insertNode(4);
 console.log('---------------------------------------------------------------');
 /*
     Find merge point of two linked lists
@@ -32,27 +32,38 @@ console.log('---------------------------------------------------------------');
 // You only need to complete this method.
 
 function findMergeNode(headA, headB) {
+    function reverse(head) {
+        let revr = null;
+        let curr = head;
+        let next = null;
+        while(curr){
+            next = curr.next;
+            curr.next = revr;
+            revr = curr;
+            curr = next;
+        }
+        return revr;
+    }
     console.log(JSON.stringify(headA));
     console.log(JSON.stringify(headB));
-    let currentA = headA.next;
-    let currentB = headB.next;
-    //Do till the two nodes are the same
-    while(JSON.stringify(currentA) != JSON.stringify(currentB)){
-        if(currentA && currentA.next == null){
-            currentA = headB;
-        }else{
+    console.log('-----------------------');
+    let promises = [Promise.resolve(reverse(headA)), Promise.resolve(reverse(headB))];
+    Promise.all(promises).then(values => {
+        let currentA = values[0];
+        let currentB = values[1];
+        console.log(JSON.stringify(currentA));
+        console.log(JSON.stringify(currentB));
+        let merged = null;
+        while(currentA && currentB && currentA.data === currentB.data){
+            merged = currentA.data;
             currentA = currentA.next;
-        }
-        if(currentB && currentB.next == null){
-            currentB = headA;
-        }else{
             currentB = currentB.next;
         }
-    }
-    return currentB ? currentB.data : null;
+        return merged ? merged : null;
+    })
 }
 console.log('---------------------------------------------------------------');
 let result = findMergeNode(llist.head, llist2.head);
 console.log(JSON.stringify(result));
-let result2 = findMergeNode(llist3.head, llist4.head);
-console.log(JSON.stringify(result2));
+// let result2 = findMergeNode(llist3.head, llist4.head);
+// console.log(JSON.stringify(result2));
