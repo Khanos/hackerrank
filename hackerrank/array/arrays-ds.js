eef9adeabf25c9a1b0363b53664d15edd13b2341
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-01 08:18:19 -0400

    this was easy

--- hackerrank/array/arrays-ds.js
+++ hackerrank/array/arrays-ds.js
@@ -1,6 +1,11 @@
 // Complete the reverseArray function below.
 function reverseArray(a) {
-
+    let arr = [];
+    for(const element of a){
+        arr.unshift(element);
+    }
+    return arr;
 }
 
 let arr = [1, 4, 3, 2]
+console.log(reverseArray(arr));
