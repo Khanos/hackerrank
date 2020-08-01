3de2eb88e47b0bea2fe16144008ad2bfef494515
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-01 12:13:35 -0400

    trying a hard one

---
+++ hackerrank/array/sparse-arrays.js
@@ -0,0 +1,7 @@
+// Complete the matchingStrings function below.
+function matchingStrings(strings, queries) {
+
+}
+let string = [ 'aba', 'baba', 'aba', 'xzxb' ];
+let queries = [ 'aba', 'xzxb', 'ab' ];
+matchingStrings(string, queries);
