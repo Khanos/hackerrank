21ebaca1e16335f6d783b6d07d590ffa97efd4d3
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-01 12:29:21 -0400

    this was quite easy

--- hackerrank/array/sparse-arrays.js
+++ hackerrank/array/sparse-arrays.js
@@ -1,7 +1,18 @@
 // Complete the matchingStrings function below.
 function matchingStrings(strings, queries) {
-
+    let count = 0;
+    let countArr = [];
+    for(const query of queries){
+        for(const string of strings){
+            if(query === string){
+                count++;
+            }
+        }
+        countArr.push(count);
+        count = 0;
+    }
+    return countArr;
 }
-let string = [ 'aba', 'baba', 'aba', 'xzxb' ];
+let strings = [ 'aba', 'baba', 'aba', 'xzxb' ];
 let queries = [ 'aba', 'xzxb', 'ab' ];
-matchingStrings(string, queries);
+console.log(matchingStrings(strings, queries));
