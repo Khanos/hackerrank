798b723c1a57df6bb041facf8e8da1aa291012f1
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-06 09:23:43 -0400

    done with: challenges/mark-and-toys/problem

--- hackerrank/sorting/mark-and-toys.js
+++ hackerrank/sorting/mark-and-toys.js
@@ -1,6 +1,20 @@
 // Complete the maximumToys function below.
 function maximumToys(prices, k) {
-
+    let budget = k;
+    let count = 0;
+    let sortedPrices = prices.sort((prev, next) => {
+        return prev-next;
+    });
+    for(const toyPrice of sortedPrices){
+        if(budget - toyPrice > 0){
+            budget -= toyPrice;
+            count++;
+        } else {
+            break;
+        }
+    }
+    return count;
 }
 let prices = [ 1, 12, 5, 111, 200, 1000, 10 ];
 let k = 50;
+console.log(maximumToys(prices, k));
