bde631e79fc1a0b339a04e8ccbdbf6328ea77304
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-07 12:19:53 -0400

    hackerank it's kind of span, but i like it

---
+++ hackerrank/Algorithms/two-arrays.js
@@ -0,0 +1,14 @@
+// Complete the twoArrays function below.
+function twoArrays(k, A, B) {
+
+
+}
+let k, A, B;
+k = 10;
+A = [ 2, 1, 3 ];
+B = [ 7, 8, 9 ];
+console.log(twoArrays(k, A, B));
+k = 5;
+A = [ 1, 2, 2, 1 ];
+B = [ 3, 3, 3, 4 ];
+console.log(twoArrays(k, A, B));
