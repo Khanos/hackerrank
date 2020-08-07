97bc86ba6b66a9d9841818534162aeee3ea41857
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-07 13:37:28 -0400

    done

--- hackerrank/Algorithms/two-arrays.js
+++ hackerrank/Algorithms/two-arrays.js
@@ -1,9 +1,26 @@
 // Complete the twoArrays function below.
+// function twoArrays(k, A, B) {
+//     let sumA = A.reduce((sum, currentValue) => sum+currentValue);
+//     let sumB = B.reduce((sum, currentValue) => sum+currentValue);
+//     let tot = (sumA + sumB) / A.length;
+//     return tot >= k ? 'YES' : 'NO'
+// }
+
+// Complete the twoArrays function below.
 function twoArrays(k, A, B) {
-    let sumA = A.reduce((sum, currentValue) => sum+currentValue);
-    let sumB = B.reduce((sum, currentValue) => sum+currentValue);
-    let tot = (sumA + sumB) / A.length;
-    return tot === k ? 'YES' : 'NO'
+    let A_ = A.sort((a,b)=>b-a);
+    let B_ = B.sort((a,b)=>a-b);
+    let pairs = [];;
+    for(const index_a in A_){
+        for(const index_b in B_){
+            if(A[index_a]+B[index_b] >= k){
+                pairs.push(`${index_a}_${index_b}`);
+                B.splice(index_b, 1);
+                break;
+            }
+        }
+    }
+    return pairs.length === A.length ? 'YES' : 'NO';
 }
 let k, A, B;
 k = 10;
@@ -15,6 +32,6 @@ A = [ 1, 2, 2, 1 ];
 B = [ 3, 3, 3, 4 ];
 console.log(twoArrays(k, A, B));
 k = 5;
-A = [ 1,1,1,1,1 ];
-B = [ 4,4,4,4,4 ];
+A = [ 1, 2, 2, 1 ];
+B = [ 3, 3, 3, 4 ];
 console.log(twoArrays(k, A, B));
