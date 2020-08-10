6795651c13e4d3497575c31a29b6f5cc7f55d258
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-10 05:58:27 -0400

    being busy, but trying my best

---
+++ hackerrank/Algorithms/grading.js
@@ -0,0 +1,30 @@
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+function gradingStudents(grades) {
+    // Write your code here
+    let getNextMultOfFive = (n) => (n - (n % 5))+ 5;
+    let finalGrades = [];
+    for(const grade of grades){
+        if(grade < 38){
+            finalGrades.push(grade);
+        } else {
+            let nextMultOfFive = getNextMultOfFive(grade);
+            if(nextMultOfFive<grade+3){
+                finalGrades.push(nextMultOfFive);
+            } else {
+                finalGrades.push(grade);
+            }
+        }
+    }
+    return finalGrades;
+}
+let grades = [ 73, 67, 38, 33 ];
+let finalGrades = gradingStudents(grades);
+for(const grade of finalGrades){
+    console.log(grade);
+}
