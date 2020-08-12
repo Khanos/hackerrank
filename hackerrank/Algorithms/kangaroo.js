4d46076e2bb178f60d828ea92d018a54a65bca43
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-12 09:46:54 -0400

    today some random

---
+++ hackerrank/Algorithms/kangaroo.js
@@ -0,0 +1,11 @@
+// Complete the kangaroo function below.
+function kangaroo(x1, v1, x2, v2) {
+
+
+}
+let x1 = 0;
+let v1 = 3;
+let x2 = 4;
+let v2 = 2;
+let result = kangaroo(x1,v1,x2,v2);
+console.log(result);
