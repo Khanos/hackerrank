613904ebbfc65d70fa5271bee33a38ae18f5097e
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-12 11:04:22 -0400

    this was tricky, put it on paper, a litle reasoning and puff

--- hackerrank/Algorithms/kangaroo.js
+++ hackerrank/Algorithms/kangaroo.js
@@ -1,11 +1,24 @@
 // Complete the kangaroo function below.
 function kangaroo(x1, v1, x2, v2) {
-
-
+    if (v1 < v2 && x1 < x2 || v1 > v2 && x1 > x2) {
+        return "NO"
+    } else {
+        if((x1 - x2) % (v2 - v1) === 0){
+            return "YES"
+        } else {
+            return "NO"
+        }
+    }
 }
 let x1 = 0;
 let v1 = 3;
 let x2 = 4;
 let v2 = 2;
-let result = kangaroo(x1,v1,x2,v2);
+let result = kangaroo(x1, v1, x2, v2);
+console.log(result);
+x1 = 0;
+v1 = 2;
+x2 = 5;
+v2 = 3;
+result = kangaroo(x1, v1, x2, v2);
 console.log(result);
