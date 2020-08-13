9db1d651bc7a121c68e7465f7713ef0bb57594c6
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-13 10:02:58 -0400

    starting late

---
+++ hackerrank/Algorithms/luck-balance.js
@@ -0,0 +1,8 @@
+// Complete the luckBalance function below.
+function luckBalance(k, contests) {
+
+}
+
+let k = 3;
+let contests = [ [ 5, 1 ], [ 2, 1 ], [ 1, 1 ], [ 8, 1 ], [ 10, 0 ], [ 5, 0 ] ];
+console.log(luckBalance(k, contests));
