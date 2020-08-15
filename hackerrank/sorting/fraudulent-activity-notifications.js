acf06d7d0be17f83f398b4caf7a14126fa8d5f60
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-14 20:31:30 -0400

    more changes

--- hackerrank/sorting/fraudulent-activity-notifications.js
+++ hackerrank/sorting/fraudulent-activity-notifications.js
@@ -2,12 +2,12 @@
 function activityNotifications(expenditure, d) {
   let notifications = 0;
   let getMedian = (arr, start, end) => {
-    let transactions = arr.slice(0, 5).sort((prev, next) => prev-next);
+    let transactions = arr.slice(start, end).sort((prev, next) => prev-next);
     let middle = parseInt(transactions.length / 2);
-    return transactions.length % 2 !== 0 ? transactions[middle + 1] : (transactions[parseInt(middle)] + transactions[parseInt(middle) + 1]) / 2
+    return transactions.length % 2 !== 0 ? transactions[middle] : (transactions[parseInt(middle)] + transactions[parseInt(middle) + 1]) / 2
   }
   for(let index = d; index < expenditure.length; index++){
-    let median = getMedian(expenditure, index, d);
+    let median = getMedian(expenditure, index-d, index);
     if(expenditure[index] >= 2 * median){
       notifications++;
     }
@@ -16,4 +16,7 @@ function activityNotifications(expenditure, d) {
 }
 let expenditure = [ 2, 3, 4, 2, 3, 6, 8, 4, 5 ];
 let d = 5;
+console.log(activityNotifications(expenditure, d));
+expenditure = [ 10, 20, 30, 40, 50 ];
+d = 3;
 console.log(activityNotifications(expenditure, d));
