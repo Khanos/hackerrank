d7175023a27cf11897e48d77ff8a37660f912c5a
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-08-23 14:04:06 -0400

    let n = parseInt(input.split('\n')[0]);

--- hackerrank/10 Days of Statistics/Day_1_Quartiles.js
+++ hackerrank/10 Days of Statistics/Day_1_Quartiles.js
@@ -1,18 +1,26 @@
 function processData(input) {
   let n = parseInt(input.split('\n')[0]);
-  let isOdd = n % 2 === 0 ? true : false;
+  let isOdd = n % 2 !== 0 ? true : false;
   let values = input.split('\n')[1].split(' ').map(element => parseInt(element));
   let orderedValues = values.sort((a,b)=> a-b);
-  let [m1, m2] = [Math.floor((n - 1) / 2), Math.ceil((n - 1) / 2)]
-  let lowerHalf = orderedValues.slice(0, m1);
-  let upperHalf = orderedValues.slice(m2+1, n);
+  let [m1, m2] = [Math.floor((n - 1) / 2), Math.ceil((n - 1) / 2)];
+  let lowerHalf, upperHalf;
+  if(isOdd){
+    lowerHalf = orderedValues.slice(0, m1);
+    upperHalf = orderedValues.slice(m2+1, n);
+  } else {
+    lowerHalf = orderedValues.slice(0, m1+1);
+    upperHalf = orderedValues.slice(m2, n);
+  }
+
   let [h1, h2] = [Math.floor((lowerHalf.length - 1) / 2), Math.ceil((lowerHalf.length - 1) / 2)];
   let Q1 = (lowerHalf[h1] + lowerHalf[h2])/2;
   let Q2 = (orderedValues[m1] + orderedValues[m2])/2;
+  Q2 = Number.isInteger(Q2) ? Q2 : Q2.toFixed(1);
   let Q3 = (upperHalf[h1] + upperHalf[h2])/2;
   console.log(Q1);
   console.log(Q2);
   console.log(Q3);
 }
-let input = '9\n3 7 8 5 12 14 21 13 18';
+let input = '6\n7, 15, 36, 39, 40, 41';
 processData(input)
