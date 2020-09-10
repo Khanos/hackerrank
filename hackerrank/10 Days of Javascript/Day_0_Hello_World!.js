bcb069b51103a4d3e76245799ed1b84b9b38a5db
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2020-09-10 07:57:40 -0400

    this should be easy

---
+++ hackerrank/10 Days of Javascript/Day_0_Hello_World!.js
@@ -0,0 +1,28 @@
+// Task
+
+// A greeting function is provided for you in the editor below. It has one parameter,
+
+// . Perform the following tasks to complete this challenge:
+
+//     Use console.log() to print Hello, World! on a new line in the console, which is also known as stdout or standard output. The code for this portion of the task is already provided in the editor.
+//     Use console.log() to print the contents of
+
+//     (i.e., the argument passed to main).
+
+// You've got this!
+
+
+/**
+ *   A line of code that prints "Hello, World!" on a new line is provided in the editor.
+ *   Write a second line of code that prints the contents of 'parameterVariable' on a new line.
+ *
+ *	Parameter:
+ *   parameterVariable - A string of text.
+ **/
+function greeting(parameterVariable) {
+  // This line prints 'Hello, World!' to the console:
+  console.log('Hello, World!');
+  console.log(parameterVariable);
+  // Write a line of code that prints parameterVariable to stdout using console.log:
+
+}
