52c3399a1a9d04f93f46a1026ce2020487a8046f
khanos <epilef.rodriguez@gmail.com>
2020-09-29 06:11:09 -0400

    types and operators: done

---
+++ 30 Days of Code/Day_2_Operators.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the solve function below.
+function solve(meal_cost, tip_percent, tax_percent) {
+    const tip = meal_cost * (tip_percent/100);
+    const tax = meal_cost * (tax_percent/100);
+    const total_cost = meal_cost + tip + tax;
+    console.log(Math.round(total_cost));
+}
+
+function main() {
+    const meal_cost = parseFloat(readLine());
+
+    const tip_percent = parseInt(readLine(), 10);
+
+    const tax_percent = parseInt(readLine(), 10);
+
+    solve(meal_cost, tip_percent, tax_percent);
+}
