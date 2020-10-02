5aed987eaba1014f04f12891cd2df22ed1b9083e
khanos <epilef.rodriguez@gmail.com>
2020-10-02 08:21:36 -0400

    another one

---
+++ 30 Days of Code/Day_3_Intro_to_Conditional_Statements.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
+function main() {
+    const N = parseInt(readLine(), 10);
+    if (N % 2 !== 0) {
+        console.log("Weird");
+    } else {
+        if (2 <= N && N <= 5) {
+            console.log("Not Weird");
+        }
+        if (6 <= N && N <= 20) {
+            console.log("Weird");
+        }
+        if (20 < N) {
+            console.log("Not Weird");
+        }
+    }
+}
