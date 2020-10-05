98d924a1c87f80b699e631cb900a80944b956d58
khanos <epilef.rodriguez@gmail.com>
2020-10-05 07:20:07 -0400

    one more variable but is more legible

--- 30 Days of Code/Day_8_Dictionaries_and_Maps.js
+++ 30 Days of Code/Day_8_Dictionaries_and_Maps.js
@@ -1,12 +1,13 @@
 function processData(input) {
   const inputArr = input.split('\n');
-  const data = inputArr.slice(1, 1 + parseInt(inputArr[0]));
+  const n = inputArr.shift();
+  const data = inputArr.slice(0, n);
   const phoneBook = data.reduce((acc, el) => {
       const keyValue = el.split(' ');
       acc[keyValue[0]] = keyValue[1];
       return acc;
   }, {});
-  const dataToCheck = inputArr.slice(1 + parseInt(inputArr[0]));
+  const dataToCheck = inputArr.slice(n);
   dataToCheck.forEach(el => {
       if (phoneBook[el]) {
           console.log(`${el}=${phoneBook[el]}`)
