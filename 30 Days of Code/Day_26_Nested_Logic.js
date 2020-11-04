b8cd47497c789f16fdced1b26a1a754be9c482ff
khanos <epilef.rodriguez@gmail.com>
2020-11-04 11:44:20 -0400

    done with this one

--- 30 Days of Code/Day_26_Nested_Logic.js
+++ 30 Days of Code/Day_26_Nested_Logic.js
@@ -2,23 +2,13 @@ function processData(input) {
   //Enter your code here
   let temp;
   temp = input.split('\n')[0].split(' ');
-  const actual = {
-    d: temp[0],
-    m: temp[1],
-    y: temp[2]
-  };
+  const actual = {d: temp[0], m: temp[1], y: temp[2]};
   temp = input.split('\n')[1].split(' ');
-  const expected = {
-    d: temp[0],
-    m: temp[1],
-    y: temp[2]
-  };
-
+  const expected = {d: temp[0], m: temp[1], y: temp[2]};
   const getFine = (actual, expected) => {
     const yearFine = 10000 * (actual.y - expected.y);
     const monthFine = 500 * (actual.m - expected.m);
     const dayFine = 15 * (actual.d - expected.d);
-
     if (yearFine === 0) {
       if (monthFine === 0) {
         return (dayFine > 0) ? dayFine : 0;
@@ -28,7 +18,6 @@ function processData(input) {
     }
     return (yearFine > 0) ? 10000 : 0;
   }
-
   const fine = getFine(actual, expected);
   console.log(fine);
 }
