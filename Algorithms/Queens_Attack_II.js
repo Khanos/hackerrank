6ddc5b736de686f57a8d5b0aa28bf39f3e1b1703
khanos <epilef.rodriguez@gmail.com>
2020-11-09 19:04:04 -0400

    latest

---
+++ Algorithms/Queens_Attack_II.js
@@ -0,0 +1,9 @@
+let n = 5;
+let k = 3
+let r_q = 4;
+let c_q = 3;
+let obstacles = [ [ 5, 5 ], [ 4, 2 ], [ 2, 3 ] ];
+// Complete the queensAttack function below.
+function queensAttack(n, k, r_q, c_q, obstacles) {
+  console.log(n, k, r_q, c_q, obstacles);
+}
