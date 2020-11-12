b5247c3f58c761a68992bfc669854a46db03bef4
khanos <epilef.rodriguez@gmail.com>
2020-11-12 12:47:29 -0400

    more, more ,more

--- Algorithms/Ice_Cream_Parlor.js
+++ Algorithms/Ice_Cream_Parlor.js
@@ -1,8 +1,27 @@
 let m = 4;
-let arr = [ 1, 4, 5, 3, 2 ];
+let arr = [ 2, 2, 4, 3 ];
 
 function icecreamParlor(m, arr) {
-
+  let pair = null;
+  for (let i=1; i<=arr.length; i++) {
+    if (arr[i-1] < m) {
+      let newArr = arr.slice(i);
+      for (let j=1; j<=newArr.length; j++) {
+        if(newArr[j-1]<m) {
+          let sum = arr[i-1] + newArr[j-1];
+          if (sum === m) {
+            pair = [i, j+i];
+            break;
+          }
+        }
+      }
+    }
+    if(pair) {
+      break;
+    }
+  }
+  return pair;
 };
 
-icecreamParlor(m, arr);
+let result = icecreamParlor(m, arr);
+console.log(result.join(" ") + "\n");
