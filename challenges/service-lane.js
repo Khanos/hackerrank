3d174a5c2bbd4d4c7b40e191dfb9f796ad87a4bc
khanos <epilef.rodriguez@gmail.com>
2021-08-12 09:47:01 -0400

    adding more challenges

---
+++ challenges/service-lane.js
@@ -0,0 +1,25 @@
+/*
+ * Complete the 'serviceLane' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. 2D_INTEGER_ARRAY cases
+ */
+
+//Complete the serviceLane function in the editor below.
+
+// serviceLane has the following parameter(s):
+
+// int n: the lenght of the array
+// int cases[t][2]: each element contains the starting and ending indices for a segment to consider, inclusive
+// Returns
+
+// int[t]: the maximum width vehicle that can pass through each segment of the service lane described
+
+function serviceLane(n, cases) {
+    // Write your code here
+
+
+
+}
