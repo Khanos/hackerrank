8d9d77ed0170e58ad1ad3c4d4aef22bcc7cdfc0b
Epilef Rodriguez <epilef.rodriguez@gmail.com>
2022-08-18 08:37:11 -0400

    cleaning code

--- 1 Week Preparation Kit/Day 2/flippingMatrix.js
+++ 1 Week Preparation Kit/Day 2/flippingMatrix.js
@@ -8,7 +8,6 @@ function flippingMatrix(matrix) {
             maxSum += Math.max(matrix[i][j], matrix[i][n-j-1], matrix[n-i-1][j], matrix[n-i-1][n-j-1])
         }
     }
-    
     return maxSum;
 }
 
